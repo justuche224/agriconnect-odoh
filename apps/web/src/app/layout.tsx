5a6219d24f67bafda087643fcf642eb9c2108146
justuch224 <justuche224@gmail.com>
2025-07-15 18:44:18 +0100

    feat: integrate Tawk.to live chat script into layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -27,6 +27,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/687692c3d85e95190d4b82ee/1j07iqjuf';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
