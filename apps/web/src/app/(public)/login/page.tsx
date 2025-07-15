9ba7d6d1090a644c0bdfd7eb29f6f2eebe6de5a2
justuch224 <justuche224@gmail.com>
2025-07-15 17:22:11 +0100

    feat: redirect to dashoard if logegd in

--- apps/web/src/app/(public)/login/page.tsx
+++ apps/web/src/app/(public)/login/page.tsx
@@ -2,10 +2,27 @@
 
 import SignInForm from "@/components/sign-in-form";
 import SignUpForm from "@/components/sign-up-form";
+import { authClient } from "@/lib/auth-client";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function LoginPage() {
   const [showSignIn, setShowSignIn] = useState(true);
+
+  const router = useRouter();
+  const { data: session, isPending } = authClient.useSession();
+  if (isPending) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <Loader className="animate-spin" />
+      </div>
+    );
+  }
+  if (session && session.user) {
+    router.push("/dashboard");
+    return null;
+  }
 
   return (
     <main
